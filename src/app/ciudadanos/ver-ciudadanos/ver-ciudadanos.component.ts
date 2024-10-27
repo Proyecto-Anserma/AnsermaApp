964875d1b574cb83b7d4a59e058e58dc3461814e
miguelramos1053 <miguelramos1053@gmail.com>
2024-10-27 17:28:16 -0500

    Se configur la conexion al backend

--- src/app/ciudadanos/ver-ciudadanos/ver-ciudadanos.component.ts
+++ src/app/ciudadanos/ver-ciudadanos/ver-ciudadanos.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit  } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
+import { ApiService } from '../../core/servicios/service';
 
 @Component({
   selector: 'app-ver-ciudadanos',
@@ -9,5 +13,25 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './ver-ciudadanos.component.css'
 })
 export class VerCiudadanosComponent {
+
+  private apiUrl = 'http://localhost:5000/consultar-ciudadanos'; // URL del backend
+  
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit(): void {
+    console.log('Componente inicializado');
+    const body = {
+       id: 1, 
+       title: "Holaaa" 
+    }
+    // Consultar ciudadanos
+    this.apiService.post('consultar-ciudadanos',body).subscribe(
+      response => {
+        console.log('Respuesta del backend:', response);
+      },
+      error => {
+        console.error('Error al consultar ciudadanos:', error);
+      });
+    }
 
 }
