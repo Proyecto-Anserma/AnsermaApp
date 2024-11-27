f80f0e0d2787d2a2aa80c6aa1a4994ccf27c12d5
amanduquia <alejandra.1701914799@ucaldas.edu.co>
2024-11-26 22:23:32 -0500

    Implementacion mapa en detalles de solicitud

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,4 +1,5 @@
-export const environment = {
-    
-    PORT: 'http://localhost:5000'
+export const environment = {    
+    PORT: 'http://localhost:5000',
+    production: false,
+    mapboxToken: 'pk.eyJ1IjoiYW1hbmR1cXVpYSIsImEiOiJjbTN5eTduZnEyMXh4MmpweG5icG11NjJ6In0.AGGb6fdFwXmQ8RseEC-u6g'     
 }
