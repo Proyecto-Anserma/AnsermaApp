40ad7843f41afbfec38709b8ff75658e7c092b90
amanduquia <alejandra.1701914799@ucaldas.edu.co>
2024-12-12 08:05:48 -0500

    Ver detalles ciudadano funciona

--- src/app/ciudadanos/detalles-ciudadano/detalles-ciudadano.component.ts
+++ src/app/ciudadanos/detalles-ciudadano/detalles-ciudadano.component.ts
@@ -1,7 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, AfterViewInit } from '@angular/core';
 import { Ciudadano } from '../../core/modelos/ciudadano.model';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
+import * as mapboxgl from 'mapbox-gl';
+import { environment } from '../../environments/environment';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 @Component({
   selector: 'app-detalles-ciudadano',
@@ -10,17 +13,55 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './detalles-ciudadano.component.html',
   styleUrl: './detalles-ciudadano.component.css'
 })
-export class DetallesCiudadanoComponent {
-
+export class DetallesCiudadanoComponent implements AfterViewInit {
   @Input() ciudadano!: Ciudadano;
+  map!: mapboxgl.Map;
 
   constructor(
-    public activeModal: NgbActiveModal,
-
+    public activeModal: NgbActiveModal
   ) { }
+
+  ngAfterViewInit(): void {
+    // Parsear la geolocalización desde el string
+    const coordinates = this.parseCoordinates(this.ciudadano.geolocalizacion);
+
+    // Inicializar el mapa con el token en la configuración
+    this.map = new mapboxgl.Map({
+      container: 'map', // ID del contenedor
+      style: 'mapbox://styles/mapbox/satellite-streets-v12',
+      center: coordinates, // Coordenadas del punto
+      zoom: 12, // Nivel de zoom
+      accessToken: environment.mapboxToken,
+      attributionControl: false
+    });
+
+    // Agregar un marcador en el punto con un ligero desplazamiento
+    new mapboxgl.Marker({ offset: [0, 0] }) 
+      .setLngLat(coordinates)
+      .addTo(this.map);
+
+    // Asegurar que el mapa se centra correctamente con un ajuste adicional
+    this.map.on('load', () => {
+      const offset: [number, number] = [0,0];
+      this.map.setCenter(coordinates);
+      this.map.panBy(offset);
+      this.map.resize();
+    });
+  }
+
+  // Función para extraer las coordenadas del string
+  private parseCoordinates(geoString: string): [number, number] {
+    const match = geoString.match(/POINT\s\(([^)]+)\)/);
+    if (!match) {
+      console.error('Formato de geolocalización inválido');
+      return [0, 0]; // Valores predeterminados en caso de error
+    }
+
+    const [lng, lat] = match[1].split(' ').map(coord => parseFloat(coord));
+    return [lng, lat];
+  }
 
   cerrarModal() {
     this.activeModal.dismiss('Modal cerrado');
   }
-
 }
