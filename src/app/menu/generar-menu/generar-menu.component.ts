510b217b22f758658125b1b5908bf33fd219a4ec
miguelramos1053 <miguelramos1053@gmail.com>
2024-12-13 18:09:58 -0500

    Se agrego boton de cerrar sesion

--- src/app/menu/generar-menu/generar-menu.component.ts
+++ src/app/menu/generar-menu/generar-menu.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../core/servicios/auth.service';
 
 @Component({
   selector: 'app-generar-menu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './generar-menu.component.html',
   styleUrl: './generar-menu.component.css'
 })
 export class GenerarMenuComponent {
+  constructor(
+    public authService: AuthService,
+    private router: Router
+  ) {}
 
+  cerrarSesion(): void {
+    this.authService.logout();
+    this.router.navigate(['/auth/login']);
+  }
 }
